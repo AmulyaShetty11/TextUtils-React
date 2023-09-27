c0769abc4834ec8afa5b335981a97c4f9090f3ca
AmulyaShetty11 <137149903+AmulyaShetty11@users.noreply.github.com>
2023-09-27 11:59:39 +0530

    first changes

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,6 +1,6 @@
 
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React from 'react';
 // import { Link } from 'react-router-dom';
 
 
@@ -18,7 +18,7 @@ export default function Navbar(props){
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href={props.link}>{props.linkName}</a>
@@ -28,8 +28,8 @@ export default function Navbar(props){
             Dropdown
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Action 1</a></li>
+            <li><a className="dropdown-item" href="#">Another action 2</a></li>
             <li><hr className="dropdown-divider" /></li>
             <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
