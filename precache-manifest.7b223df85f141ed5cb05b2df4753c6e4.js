54d88f478004b6d8525686be81dcd7a361c9a2aa
AmulyaShetty11 <137149903+AmulyaShetty11@users.noreply.github.com>
2023-09-27 11:52:10 +0530

    Updates

---
+++ precache-manifest.7b223df85f141ed5cb05b2df4753c6e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da8e2a579386b147c0c2",
+    "url": "/TextUtils-React/static/css/main.71e05ddc.chunk.css"
+  },
+  {
+    "revision": "da8e2a579386b147c0c2",
+    "url": "/TextUtils-React/static/js/main.da8e2a57.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "2591e466b5f270c0c477",
+    "url": "/TextUtils-React/static/js/runtime~main.2591e466.js"
+  },
+  {
+    "revision": "6299cd0318ffcec95050768169599240",
+    "url": "/TextUtils-React/index.html"
+  }
+];
